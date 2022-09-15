0d305e969631c076f1647bbe1331533a8bd691ef
bshenoy <bshenoy12@gmail.com>
2022-09-15 10:30:38 +0530

    added

--- app.py
+++ app.py
@@ -4,17 +4,6 @@ from insurance.exception import InsuranceException
 import sys, os
 
 app = Flask(__name__)
-
-try:
-    logging.info("yhhhh")
-    # raise Exception("we are tseting")
-    logging.info("trying")
-    
-except Exception as e:
-    cement = InsuranceException(e, sys)
-    logging.info(cement.error_message)
-
-
 @app.route('/', methods=['POST', 'GET'])
 def welcome():
     return "hello"
